00a6c783d8733765d1254ee98808f8da8fd26c17
Kurt Bendl <kurt@tool.net>
2013-06-21 13:18:44 -0600

    Make the backup script a bit more portable.

--- scripts/plonesitebak.sh
+++ scripts/plonesitebak.sh
@@ -2,14 +2,16 @@
 PATHS=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=PATHS:$PATH
 
+PROJ=atotool
+TARGET=kbendl@stc-17111s.nrel.gov
+
 ### Tarball and copy the production files to a local backup repo
 ### for quick recovery.
-
 TD=`date +%Y%m%d%H%m`
-cd /opt/sites/atotool/atotool-buildout/var
-../bin/backup
-sleep 1
-tar -czf var-backup.tgz backups blobstoragebackups
+cd /opt/sites/$PROJ/$PROJ-buildout/var
+../bin/fullbackup
+sleep 5
+tar -czf var-fullbackup.tgz backups blobstoragebackups
 sleep 30
-echo -e "Copying var-backup.tgz to var-backup-$TD.tgz"
-scp var-backup.tgz kbendl@stc-17111s.nrel.gov:/web/atotool/data/atotool-var-backup-$TD.tgz
+echo -e "Copying var-fullbackup.tgz to var-fullbackup-$TD.tgz"
+scp var-fullbackup.tgz $TARGET:/web/$PROJ/data/$PROJ-var-fullbackup-$TD.tgz
